52a424e5017030139f2c7d9cdde47e25eda670d5
Irhas <muhammadirhasalh@gmail.com>
2025-06-02 20:07:43 +0700

    chore: bump up to 0.2.0+19 (#73)

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kVersion = '0.1.12+18';
+const kVersion = '0.2.0+19';
 const kPrivacy = 'https://irhas.lol/app/create-invoice';
 const kPx = EdgeInsets.symmetric(horizontal: 16.0);
