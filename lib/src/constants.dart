7b2166005aa62e65dc00c486b8d47c704d06f44c
Irhas <muhammadirhasalh@gmail.com>
2025-06-03 15:13:36 +0700

    Chore/bump up version to 0.2.2+21 (#79)
    * chore: update version to 0.2.2+21
    
    * chore: update CHANGELOG for version 0.2.2

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kVersion = '0.2.1+20';
+const kVersion = '0.2.2+21';
 const kPrivacy = 'https://irhas.lol/app/create-invoice';
 const kPx = EdgeInsets.symmetric(horizontal: 16.0);
